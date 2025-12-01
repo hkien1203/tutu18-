f647aa01569122ad5a2d0efcef9bd9a1741ba07f
hkien1203 <nguyenhoangkien1203@gmail.com>
2025-12-01 15:34:55 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// 1. Menu Mobile
+function toggleMenu() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// 2. Modal Mua Vé
+function openModal() {
+    document.getElementById('ticketModal').style.display='block';
+}
+
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
